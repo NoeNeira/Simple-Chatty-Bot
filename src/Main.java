532e8cb44293108f4e869d8a62f6d7b9163e0dad
Noelia Neira <noeliasneira@gmail.com>
2020-03-14 12:35:59 -0300

    Chatty bot finished

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
 
-        System.out.println("Hello! My name is RobotName.");
+        System.out.println("Hello! My name is RoboTito.");
         System.out.println("I was created in 2020.");
         System.out.println("Please, remind me your name.");
 
